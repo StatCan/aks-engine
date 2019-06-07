16398e048a96f80bf7c9f11e4b8e23e74f4ee4e8
Zachary Seguin <zachary.seguin@canada.ca>
2019-06-07 15:53:37 -0400

    Add Windows Join Domain extension

---
+++ extensions/joindomain-windows/v1/joinDomain.ps1
@@ -0,0 +1,31 @@
+﻿#
+# Optional parameters to this script file.
+#
+
+[CmdletBinding()]
+param(
+    # comma- or semicolon-separated list of Chocolatey packages.
+    [string] $DomainName,
+    [string] $JoinUser,
+    [string] $JoinPassword,
+    [string] $OU,
+    [int] $PSVersionRequired = 3
+)
+
+###################################################################################################
+$JoinPasswordSecure = ConvertTo-SecureString $JoinPassword -AsPlainText -Force
+
+$credential = New-Object System.Management.Automation.PSCredential($JoinUser, $JoinPasswordSecure)
+if($OU) {
+    [Microsoft.PowerShell.Commands.ComputerChangeInfo]$computerChangeInfo = Add-Computer -ComputerName $VmName -DomainName $DomainName -Credential $credential -OUPath $OU -Force -PassThru
+} else {
+    [Microsoft.PowerShell.Commands.ComputerChangeInfo]$computerChangeInfo = Add-Computer -ComputerName $VmName -DomainName $DomainName -Credential $credential -Force -PassThru
+}
+if ($computerChangeInfo.HasSucceeded)
+{
+    Write-Output "Result: Successfully joined the $DomaintoJoin domain"
+}
+else
+{
+    Write-Error "Result: Failed to join $env:COMPUTERNAME to $DomaintoJoin domain"
+}
